2e94de3ac6d87c5c3e9bbc9aadf74e5f2e951ab4
Sarvesh Kulkarni <79219793+sarvesh123305@users.noreply.github.com>
2021-10-26 18:27:34 +0530

    Initial commit

---
+++ kkk.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Student Information</title>
+  </head>
+  <style>
+    h3 {
+      text-align: center;
+    }
+  </style>
+ 
+  <body bgcolor="pink">
+    <h1 align="center" style="background-color: tomato">
+      Welcome to the IT World
+    </h1>
+    <h3>Name :<?php echo $_GET['name']; ?></h3>
+    <h3>Roll No :<?php echo  $_GET['Email'];?></h3>
+    <h3>College Name :<?php echo $_GET['Roll'];?></h3>
+  </body>
+</html>
